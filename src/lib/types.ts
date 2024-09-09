b17b8c867a5d70e9ba55dc649f1caa1465e355b0
ruehan <node.js_@kakao.com>
2024-09-09 16:20:46 +0900

    fix : 백엔드 변경에 따른 type 수정

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,12 +1,31 @@
+export interface EmotionScores {
+  angry: number;
+  disgust: number;
+  fear: number;
+  happy: number;
+  sad: number;
+  surprise: number;
+  neutral: number;
+}
+
 export interface Track {
   name: string;
   artist: string;
   preview_url: string | null;
   external_url: string;
+  popularity: number;
+  audio_features: AudioFeatures;
+}
+
+export interface AudioFeatures {
+  valence: number;
+  energy: number;
+  danceability: number;
+  tempo: number;
 }
 
 export interface AnalysisResult {
-  emotion: string;
-  confidence: number;
+  dominant_emotion: string;
+  emotion_scores: EmotionScores;
   tracks: Track[];
 }
